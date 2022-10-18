739712732191e048f4c11959d287fb072389b1ed
Kplukas <p.kristupas.p@gmail.com>
2022-10-18 12:46:27 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(123)
+console.log('Labas rytas')
+
+/*
+Esmines JS temos:
+-kintamieji - informacijos isiminimas
+-salygos sakiniai - palyginimas/ patikrinimas
+-ciklai - pasikartojanciu proceduru kartojimas ki kol reikia
+-funkcijos/klases - pasikartojancios logikos surinkimas i viena vieta
+-event listeners - (fronto) stebejimas kada ir koki veiksma padare vartotojas 
+*/
---
+++ js/01-numbers.js
@@ -0,0 +1,21 @@
+/*
+KINTAMIEJI
+iniciavimo budai:
+-const (default) - reiksme nekinta
+- let - kai reiksme turi kisti
+-var - nenaudoti
+*/
+
+console.log('Numbers...')
+
+const a = 0;
+let b = 1;
+let c = 2;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+b = 5;
+
+console.log(b);
