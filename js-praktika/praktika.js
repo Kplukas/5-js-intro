c4519ae8c6c0127b90a06b0032db65bdbc1a2db5
Kplukas <p.kristupas.p@gmail.com>
2022-10-24 13:39:39 +0300

    Praktika

---
+++ js-praktika/praktika.js
@@ -0,0 +1,14 @@
+/* Kintamuju inicijavimas */
+
+let a = 1
+let b = 2
+let c = 3
+
+console.log (a , b , c)
+
+let zodis1 = 'zodisvienas'
+let zodis2 = 'zodisdu'
+let zodis3 = 'zodistrys'
+console.log (zodis1 , zodis2 , zodis3)
+
+
