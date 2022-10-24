cb74a6aa53b20e1636ac90cde307a60ceb7b34fe
Kplukas <p.kristupas.p@gmail.com>
2022-10-24 14:41:53 +0300

    praktika

--- js-praktika/praktika.js
+++ js-praktika/praktika.js
@@ -1,3 +1,5 @@
+console.clear();
+
 /* Kintamuju inicijavimas */
 
 let a = 1
@@ -9,6 +11,36 @@ console.log (a , b , c)
 let zodis1 = 'zodisvienas'
 let zodis2 = 'zodisdu'
 let zodis3 = 'zodistrys'
+
 console.log (zodis1 , zodis2 , zodis3)
+
+const sarasas1 = [1, 2, 3, 4, 5]
+
+console.log (sarasas1)
+
+const sarasas2 = ['vien' , 'du' , 'trys', 'ketur', 'penk']
+
+console.log (sarasas2)
+
+/* Veiksmai su kintamaisiais */
+
+const d = (a + b + c)
+
+console.log (d);
+console.log (a, '+', b, '+', c ,'=', d)
+
+const sakinys = (zodis1 + zodis2 + zodis3)
+console.log (sakinys)
+
+const sakinys2 = (zodis1 + ' '+ zodis2 + ' ' + zodis3 + '.')
+console.log(sakinys2);
+
+const f = [1, 2, 3, 4, 5]
+
+const g = (f[0] - f[1] + f[2] - f[3] + f[4])
+
+console.log (g);
+
+console.log (f[0], '-', f[1], '+', f[2], '-', f[3], '+', f[4], '=', g)
 
 
